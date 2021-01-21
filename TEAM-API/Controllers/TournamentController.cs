64a9321fd4aac2a718e17cbd52b09cf9e14cb567
H434400 <H434400@IE4LLTH8JNK63>
2021-01-21 15:27:44 +0530

    TournamentController initial checkin

---
+++ TEAM-API/Controllers/TournamentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TEAM_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TournamentController : ControllerBase
+    {
+        private readonly ILogger<TournamentController> _logger;
+        public TournamentController(ILogger<TournamentController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
