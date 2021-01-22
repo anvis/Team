25941d81599cc9120663964162cb7352e112c0d4
H434400 <H434400@IE4LLTH8JNK63>
2021-01-22 14:27:48 +0530

    Team Model Validation.

--- EntityLayer/Team.cs
+++ EntityLayer/Team.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EntityLayer
@@ -7,8 +8,14 @@ namespace EntityLayer
     public class Team
     {
         public int TeamId { get; set; }
+        [Required(ErrorMessage = "TeamName is required")]
+        [StringLength(50)]
         public string TeamName { get; set; }
+        [Required(ErrorMessage = "TournmentCode is required")]
+        [StringLength(10)]
         public string TournmentCode { get; set; }
+        [Required(ErrorMessage = "JerseyColor is required")]
+        [StringLength(30)]
         public string JerseyColor { get; set; }
         public List<Player> Players { get; set; }
     }
